73f75e5eea90045d28033fe3148b0cc166a8b8e0
kjdgac <kelly.davis@gulfstream.com>
2018-05-16 14:07:46 -0400

    updates booking.js to improve filtering

--- docs/booking.js
+++ docs/booking.js
@@ -14,7 +14,8 @@ var end = bookingData.end || 0;
 var qTerms = bookingData.terms || "";
 
 // where is our API?
-var ajaxSrc = "http://dev.nandointeractive.com/mugshots/";
+var ajaxSrc = //"http://dev.nandointeractive.com/mugshots/beaufort/";
+  "./php/server/mugshotsV2.php";
 
 // for filtering purposes, create an array of stringified detainee data
 var filterSource = [];
@@ -203,12 +204,12 @@ function displayInmates(data, start, end, terms) {
     var detainee = data[i];
 
     // add stringified version to filterSource
-    filterSource.push(JSON.stringify(detainee));
+    filterSource.push(JSON.stringify(detainee).toLowerCase());
 
     // build content block
     var inmateBlock =
-      '<div data-index="' +
-      i +
+      '<div data-booknum="' +
+      detainee.booknum +
       '" class="detaineeIndex col-lg-2 col-md-2 col-sm-4 col-xs-6">';
 
     // start mugshot/details row
@@ -265,7 +266,7 @@ function displayInmates(data, start, end, terms) {
   $(".detaineeIndex").on("click", function() {
     // place inmate data and search terms in localStorage
     var bookingData = {
-      inmate: data[$(this).attr("data-index")],
+      inmate: data[$(this).attr("data-booknum")],
       terms: encodeURI(
         $("#filterInput")
           .val()
@@ -593,9 +594,7 @@ function displayInmate(inmate) {
 */
 function runFilter(term) {
   // update local storage
-
   var bookingData = JSON.parse(localStorage.getItem("bcBookingData")) || {};
-
   bookingData.terms = term;
   localStorage.setItem("bcBookingData", JSON.stringify(bookingData));
 
@@ -611,16 +610,20 @@ function runFilter(term) {
     .replace(/\s{2,}/g, " ")
     .toLowerCase()
     .split(" ");
+
   // filterSource is an array of strings of values associated with each detainee
   $(filterSource).each(function(index, detValues) {
     // detValues represents the the value string for each detainee object
     // reset isMatched flag
     var isMatched = false;
 
+    // parse string so we can access a couple values
+    var detainee = JSON.parse(detValues);
+
     // check this detail string against each term
     for (var t = 0; t < terms.length; t++) {
       // does the filter input value match any of this detainee's values?
-      if (detValues.toLowerCase().indexOf(terms[t]) === -1) {
+      if (detValues.indexOf(terms[t]) === -1) {
         // no? set isMatched to false, exit loop because there's no need to keep searching
         isMatched = false;
         break;
@@ -628,25 +631,22 @@ function runFilter(term) {
       // yes?
       ///set isMatched to true but keep checking other words in value array
       else {
+        isMatched = true;
         // make sure we don't match female with male
-        if (
-          terms[t] === "male" &&
-          JSON.parse(detValues).sex.toLowerCase() === "female"
-        ) {
+        if (terms[t] === "male" && detainee.sex.toLowerCase() === "female") {
           isMatched = false;
-        } else {
-          isMatched = true;
         }
       }
     }
 
     //after checking this element against each term currently in filter, is isMatched still true?
+
     if (isMatched) {
-      $(".detaineeIndex[data-index='" + index + "']").show("fast");
+      $(".detaineeIndex[data-booknum='" + detainee.booknum + "']").show("fast");
     }
     // otherwise, hide it
     else {
-      $(".detaineeIndex[data-index='" + index + "']").hide("fast");
+      $(".detaineeIndex[data-booknum='" + detainee.booknum + "']").hide("fast");
     }
   });
   // when filtering loop completes, remove spinner from input
